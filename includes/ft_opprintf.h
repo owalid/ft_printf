0776e9a7795514761fde3de569e5412f3c722872
Theo DERVILLE <thdervil@e2r9p9.42.fr>
2019-02-07 17:24:19 +0100

    s

--- includes/ft_opprintf.h
+++ includes/ft_opprintf.h
@@ -6,6 +6,15 @@
 static t_prtfop g_prtfop[] =
 {
 	{'d', &ft_printnbr},
+	{'i', &ft_printnbr},
+	{'o',
+	{'u',
+	{'x',
+	{'X',
+	{'c', &
+	{'s',
+	{'p',
+
 	{'s', &ft_printstr},
 	{'\0', &ft_printnbr},
 };
