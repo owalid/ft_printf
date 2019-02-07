e2b2b89904ddf961a4a5252ca9945e160649170e
Theo DERVILLE <thdervil@e2r9p9.42.fr>
2019-02-07 17:22:14 +0100

    s

--- srcs/ft_operations.c
+++ srcs/ft_operations.c
@@ -1,6 +1,6 @@
 #include "ft_printf.h"
 
-void			ft_printnbr(void *nbr)
+void		ft_printnbr(void *nbr)
 {
 	char	*str;
 	int		i;
@@ -8,6 +8,16 @@ void			ft_printnbr(void *nbr)
 	i = -1;
 	(int)nbr;
 	str = ft_itoa(nbr);
+	while (str[++i])
+		add_char(str[i], 0);
+}
+
+void		ft_printstr(void *str)
+{
+	int	i;
+
+	i = -1;
+	(char*)str;
 	while (str[++i])
 		add_char(str[i], 0);
 }
