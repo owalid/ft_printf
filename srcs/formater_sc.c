832f1c1b02ffbb6bf7864927d841d734eb8e22fd
Théo Derville <derachte@MacBook-Pro-de-Theo.local>
2019-05-14 09:51:00 +0200

    Attends jfais encore dla merde

--- srcs/formater_sc.c
+++ srcs/formater_sc.c
@@ -48,84 +48,16 @@ int		ft_formater_sc(t_output *output, int opt)
 	size_t	i;
 	size_t	size;
 	
-<<<<<<< HEAD
-	result = NULL;
-	if (!output->str)
-=======
 	if (!output->str || output->size_flag->no_prec)
->>>>>>> origin/owalid
 	{
 		output->is_null = 1;
 		output->str = ft_strdup("(null) du ft_formater_sc");
 	}
-<<<<<<< HEAD
-	output->option->space == 1 ? output->option->plus = 0 : 0;
-=======
 	output->minsize = (output->minsize <= 0 && !output->size_flag->precision) ? ft_strlen(output->str) : output->minsize;
->>>>>>> origin/owalid
 	size = output->minsize + output->option->space + output->option->plus;
 	if (size < 0)
 		size = 1;
 	i = 0;
-<<<<<<< HEAD
-	if (output->conv_type == 'c')
-	{
-		result = ft_formater_with_option_sc(output, size);
-		if (output->option->min == 1 && output->minsize > ft_strlen(result))
-		{
-			tmp = ft_strnew(output->minsize - ft_strlen(result));
-			while (i < output->minsize - ft_strlen(result))
-				tmp[i++] = ' ';
-			if (i != 0)
-				result = ft_strjoin(result, tmp);
-			ft_strdel(&tmp);	
-		}
-		else
-			result = ft_formater_with_option_s(output, ft_strlen(output->str));
-		if (output->option->space == 1)
-		{
-			if (output->minsize > ft_strlen(result))
-			{
-				i = 0;
-				tmp = ft_strnew(output->minsize - ft_strlen(result));
-				while (i < output->minsize - ft_strlen(result))
-					tmp[i++] = ' ';
-				if (i != 0)
-					result = ft_strjoin(tmp, result);
-				ft_strdel(&tmp);
-			}
-			else
-			{
-				tmp = ft_str_from_char(' ');
-				result = ft_strjoin(tmp, result);
-				ft_strdel(&tmp);
-			}
-		}
-		if (output->minsize > ft_strlen(result) && !output->option->min)
-		{
-			i = 0;
-			tmp = ft_strnew(output->minsize - ft_strlen(result));
-			while (i < output->minsize - ft_strlen(result))
-				tmp[i++] = ' ';
-			if (i != 0)
-				result = ft_strjoin(tmp, result);
-			ft_strdel(&tmp);
-		}
-	}
-	if (output->minsize > ft_strlen(result) && !output->option->min)
-	{
-		i = 0;
-		tmp = ft_strnew(output->minsize  - ft_strlen(result));
-		while (i < output->minsize  - ft_strlen(result))
-			tmp[i++] = ' ';
-		if (i != 0)
-			result = ft_strjoin(tmp, result);
-		ft_strdel(&tmp);
-	}
-	i = -1;
-	while (result[++i])
-		add_char(result[i], opt);
-=======
 	if (ft_strlen(output->str) < size)
 	{
 		output->str = ft_formater_with_option_sc(output, size);
@@ -141,6 +73,5 @@ int		ft_formater_sc(t_output *output, int opt)
 	send_char(output->str, opt, &i);
 	if (output->size_flag->no_prec)
 		i = 0;
->>>>>>> origin/owalid
 	return (i);
 }
